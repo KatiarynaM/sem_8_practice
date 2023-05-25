df693be84a1154bce4111ffa4d315992df14f4e5
Katya.M <119962546+KatiarynaM@users.noreply.github.com>
2023-05-25 22:48:45 +0300

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,18 +1,71 @@
 def show_data() -> None:
     """Выводит информацию из справочника"""
-    pass
+    with open('book.txt', 'r', encoding='utf-8') as book:
+        print(book.read())
 
 
 def add_data() -> None:
     """Добавляет информацию в справочник."""
-    pass
+    fio = input('Введите ФИО: ')
+    phone_num = input('Введите номер телефона: ')
+    with open('book.txt', 'a', encoding='utf-8') as book:
+        book.write(f'\n{fio} | {phone_num}')
 
 
 def find_data() -> None:
     """Печатает результат поиска по справочнику."""
-    pass
+    with open('book.txt', 'r', encoding='utf-8') as file:
+        data = file.read()
+    contact_to_find = input('Введите, что хотите найти: ')
+    result = search(data, contact_to_find)
+    print(result)
 
 
-def search(book: str, info: str) -> str:
+def search(book: str, info: str) -> list[str]:
     """Находит в списке записи по определенному критерию поиска"""
-    pass
+    book = book.split('\n')
+    return list(filter(lambda contact: info.lower() in contact.lower(), book))
+
+#or
+''' lines = book.splitlines()
+    res = []
+    for line in lines:
+        if info in line:
+            res.append(line)
+    return res'''
+
+#or
+#book = book.split('\n')
+#  return [contact for contact in book if info.lower() in contact.lower()]
+ 
+
+def delet_data() -> None:
+    """Удаляет результат поиска по справочнику."""
+    with open('book.txt', 'w+', encoding='utf-8') as file:
+        data = file.read()
+    contact_to_find = input('Введите, что хотите удалить:  ')
+    result = search(data, contact_to_find)
+    print(result)
+    if len(result) == 1:
+        result.write()
+    elif len(result) > 1:
+        print('Уточните...')
+    else:
+        print('Ничего не найдено')
+    
+def change_data() -> None:
+    """Изменяет результат поиска по справочнику."""
+    with open('book.txt', 'w+', encoding='utf-8') as file:
+    data = file.read()
+    contact_to_find = input('Введите, что хотите изменить:  ')
+    result = search(data, contact_to_find)
+    print(result)
+    if len(result) == 1:
+        fio = input('Введите ФИО: ')
+        phone_num = input('Введите номер телефона: ')
+        with open('book.txt', 'a', encoding='utf-8') as book:
+            book.write(f'\n{fio} | {phone_num}')
+    elif len(result) > 1:
+        print('Уточните...')
+    else:
+        print('Ничего не найдено')
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import functions
 
 
 while True:
-    print('1. вывод, 2. добавление, 3. поиск')
+    print('1. вывод, 2. добавление, 3. поиск, 4. удаление, 5. изменение')
     mode = int(input())
     if mode == 1:
         functions.show_data()
@@ -20,5 +20,9 @@ while True:
         functions.add_data()
     elif mode == 3:
         functions.find_data()
+    elif mode == 4:
+        functions.delet_data()
+    elif mode == 5:
+        functions.change_data()  
     else:
         break
