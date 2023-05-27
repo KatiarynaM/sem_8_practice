ac7e9e2a64a282cca96d36add1a2af6970e33121
Katya.M <119962546+KatiarynaM@users.noreply.github.com>
2023-05-27 12:27:34 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -21,51 +21,38 @@ def find_data() -> None:
     print(result)
 
 
-def search(book: str, info: str) -> list[str]:
+def search(book: list[str], info: str) -> list[str] | str:
     """Находит в списке записи по определенному критерию поиска"""
-    book = book.split('\n')
-    return list(filter(lambda contact: info.lower() in contact.lower(), book))
+   
+    result = [contact for contact in book if info.lower() in contact.lower()]
+    if len(result) == 1:
+        return result[0]
+    elif len(result) > 1:
+        print('Уточните...')
+    else:
+        print('Ничего не найдено')
 
-#or
-''' lines = book.splitlines()
-    res = []
-    for line in lines:
-        if info in line:
-            res.append(line)
-    return res'''
-
-#or
-#book = book.split('\n')
-#  return [contact for contact in book if info.lower() in contact.lower()]
- 
 
 def delet_data() -> None:
     """Удаляет результат поиска по справочнику."""
     with open('book.txt', 'r', encoding='utf-8') as file:
-        data = file.read()
-    contact_to_find = input('Введите, что хотите удалить:  ')
-    result = search(data, contact_to_find)
-    print(result)
-    if len(result) == 1:
-        result.write()
-    elif len(result) > 1:
-        print('Уточните...')
-    else:
-        print('Ничего не найдено')
-    
+        data = file.read().split('\n')
+    contact_to_delet = input('Введите, что хотите удалить:  ')
+    print(contact_to_delet)
+    contact_to_delet = search(data, contact_to_delet)
+    data.remove(contact_to_delet)
+    with open('book.txt', 'w', encoding='utf-8') as file:
+        file.write('\n'.join(data))
+
 def change_data() -> None:
     """Изменяет результат поиска по справочнику."""
-    with open('book.txt', 'r+', encoding='utf-8') as file:
-    data = file.read()
-    contact_to_find = input('Введите, что хотите изменить:  ')
-    result = search(data, contact_to_find)
-    print(result)
-    if len(result) == 1:
-        fio = input('Измените ФИО: ')
-        phone_num = input('Измените номер телефона: ')
-        with open('book.txt', 'a', encoding='utf-8') as book:
-            book.write(f'\n{fio} | {phone_num}')
-    elif len(result) > 1:
-        print('Уточните...')
-    else:
-        print('Ничего не найдено')
+    with open('book.txt', 'r', encoding='utf-8') as file:
+        data = file.read().split('\n')
+    contact_to_change = input('Введите, что хотите изменить:  ')
+    contact_to_change = search(data, contact_to_change)
+    print(contact_to_change)
+    fio = input('Введите ФИО: ')
+    phone_num = input('Введите номер телефона: ')
+    data[data.index(contact_to_change)] = f'{fio} | {phone_num}'
+    with open('book.txt', 'w', encoding='utf-8') as file:
+        file.write('\n'.join(data))
